5064a750996b97e8f2b52a0df3e3f6703fd4401c
Park Seo Yeon <syss220211@gmail.com>
2023-11-23 11:52:52 +0900

    style: 주석 추가

--- Projects/App/Sources/SY/Location/LocationManager.swift
+++ Projects/App/Sources/SY/Location/LocationManager.swift
@@ -9,16 +9,25 @@ struct Location {
 }
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private var locationManager = CLLocationManager()
-    var userLocationInto = Location(crdntX: 0.0, crdntY: 0.0)
     
-    @Published var userLocation: CLLocation?
+    private var locationManager = CLLocationManager()
+    
     @Published var locationString: String = "Fetching location..."
+    @Published var userLocationInto = Location(crdntX: 0.0, crdntY: 0.0)
+    @Published var distacneArray: [Double] = []
+    @Published var stationArray: [String] = []
     
     override init() {
         super.init()
+        // CLLocationManager의 대리자를 self로 설정 -> LocationManager 클래스가 위치 서비스와 관련된 업데이트 및 이벤트를 처리한다는 것을 나타냄
         self.locationManager.delegate = self
+        
+        // 포그라운드에 있는동안 위치 서비스를 사용할 수 있는 권한을 사용자에게 요청 (개인정보와 관련하여.. 앱 사용중일 때 위치 정보에 엑세스 할수 있다는 의미)
+        // 해당 항목은 plist의 privacy 키값에 의해 제어됨
         self.locationManager.requestWhenInUseAuthorization()
+        
+        // 대리인(self)에게 위치 없데이트 전달을 시작함 -> delegate 채택으로 인하여 정의한 didUpdateLocations 대리자 메서드는 사용 가능한 새 위치 데이터가 있을 때마다 호출됩니다.
+
         self.locationManager.startUpdatingLocation()
     }
     
@@ -26,49 +35,33 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         self.locationManager.requestLocation()
     }
     
+    /// delegate 관련 정의 함수
+    /// 이 메소드는 새로운 위치 데이터를 사용할 수 있을 때 호출됩니다. 사용자의 업데이트된 위치를 나타내는 CLLocation 개체 배열을 제공합니다.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let userLocation = locations.first else { return }
-        self.userLocation = userLocation
+        guard let userLocation = locations.first else { return } // 사용자 위치가 nil인지 아닌지 판단
+
         manager.stopUpdatingLocation()
-        
-        // Display latitude and longitude
+
+        // 위도(latitude-37)와 경도(longitude-126) 추출하기
         let latitude = userLocation.coordinate.latitude
         let longitude = userLocation.coordinate.longitude
         locationString = "Latitude: \(latitude), Longitude: \(longitude)"
-        
+
         userLocationInto.crdntY = latitude
         userLocationInto.crdntX = longitude
     }
     
+    /// delegate 관련 정의 함수
+    /// 위치 관리자가 사용자의 위치를 ​​가져오는 중 오류가 발생하면 이 메소드가 호출됩니다.
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Location error: \(error.localizedDescription)")
         locationString = "Failed to fetch location"
     }
     
-//    func calculateDistance(userLocation: Location, targetLocation: StationLocation) -> Double? {
-//        let userLatitude = Double(userLocation.crdntY)
-//        let userLongitude = Double(userLocation.crdntX)
-//        let targetLatitude = Double(targetLocation.crdntY)
-//        let targetLongitude = Double(targetLocation.crdntX)
-//
-//        let earthRadius: Double = 6371 // Radius of the Earth in kilometers
-//
-//        let deltaLat = (targetLatitude - userLatitude).toRadians()
-//        let deltaLon = (targetLongitude - userLongitude).toRadians()
-//
-//        let a = sin(deltaLat / 2) * sin(deltaLat / 2) +
-//                cos(userLatitude.toRadians()) * cos(targetLatitude.toRadians()) *
-//                sin(deltaLon / 2) * sin(deltaLon / 2)
-//
-//        let c = 2 * atan2(sqrt(a), sqrt(1 - a))
-//
-//        let distance = earthRadius * c
-//        return distance
-//    }
-    
+    /// 위경도 값 기준 가장 가까운 거리의 역 찾기
     func findClosestStation(userLocation: Location, stationLocations: [StationLocation]) -> StationLocation? {
         guard !stationLocations.isEmpty else {
-            return nil // Return nil if there are no stations
+            return nil
         }
 
         var closestStation: StationLocation?
@@ -83,15 +76,13 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
             if totalDifference < minDifference {
                 minDifference = totalDifference
                 closestStation = station
+                self.distacneArray.append(totalDifference)
+//                self.stationArray.append(closestStation?.statnNm ?? "⭐️")
             }
+            self.stationArray.append(closestStation?.statnNm ?? "⭐️")
+            
         }
-        
         return closestStation
     }
-}
 
-extension Double {
-    func toRadians() -> Double {
-        return self * .pi / 180.0
-    }
 }
--- Projects/App/Sources/SY/Location/LocationViewMain.swift
+++ Projects/App/Sources/SY/Location/LocationViewMain.swift
@@ -2,6 +2,7 @@
 
 import SwiftUI
 
+@MainActor
 class LocationViewModel: ObservableObject {
     private let firestoreManager = FirebaseLocationManager.shared
     @Published var documentsData: [StationLocation] = []
@@ -24,17 +25,6 @@ struct LocationViewMain: View {
     @StateObject var locationManager = LocationManager()
     
     @State var closestStation: StationLocation?
-//    @State var minDistance: Double?
-//
-//    func findData() {
-//        for station in LocationVM.documentsData {
-//            if let distance = locationManager.calculateDistance(userLocation: locationManager.userLocationInto, targetLocation: station),
-//                minDistance == nil || distance < minDistance! {
-//                minDistance = distance
-//                closestStation = station
-//            }
-//        }
-//    }
     
     func findStation() {
         self.closestStation = locationManager.findClosestStation(userLocation: locationManager.userLocationInto, stationLocations: LocationVM.documentsData)
@@ -43,37 +33,10 @@ struct LocationViewMain: View {
     
     var body: some View {
         VStack {
-            Button {
-                LocationVM.fetchingData()
-//                print(LocationVM.documentsData)
-            } label: {
-                Text("Fetch Button")
-            }
-            
-            Button {
-                locationManager.fetchUserLocation()
-                print(locationManager.userLocationInto)
-            } label: {
-                Text("위치버튼")
-            }
-            
+            Text("유저 위치값")
             Text(locationManager.locationString)
-            
-//            Button {
-//                findData()
-//
-//                if let closestStation = closestStation, let minDistance = minDistance {
-//                    print("Closest station is \(closestStation.statnNm) with a distance of \(minDistance) km.")
-//                } else {
-//                    print("Unable to determine the closest station.")
-//                }
-//            } label: {
-//                Text("find!")
-//            }
 
             Button {
-                print(locationManager.userLocationInto)
-                print(LocationVM.documentsData)
                 if let findLocation = locationManager.findClosestStation(userLocation: locationManager.userLocationInto, stationLocations: LocationVM.documentsData) {
                     print("😎\(findLocation.statnNm)")
                 } else {
@@ -82,6 +45,18 @@ struct LocationViewMain: View {
             } label: {
                 Text("find!")
             }
+            
+            Button {
+                print(locationManager.distacneArray.sorted())
+                print(locationManager.stationArray)
+            } label: {
+                Text("거리 배열 값 확인하기")
+            }
+
+        }
+        .onAppear {
+            LocationVM.fetchingData()
+            locationManager.fetchUserLocation()
         }
     }
 }
