db90c4d9c114c2e32bf5c97387ec064acf7d8889
Park Seo Yeon <syss220211@gmail.com>
2023-12-04 21:34:35 +0900

    feat: 설정뷰 구현 완료
    설정뷰에서 다크모드 설정하는거 구현 완료했습니다.
    앱 버전 및 컨택 쪽은 어떤 내용인지 아직 미정인것 같아서 뷰만 만들어놓았어요.

--- Projects/App/Sources/Views/SettingView.swift
+++ Projects/App/Sources/Views/SettingView.swift
@@ -7,6 +7,7 @@ enum SchemeType: Int, CaseIterable {
     case light
     case dark
     
+    /// 피커에 들어갈 말들
     var schemeType: String {
         switch self {
         case .system:
@@ -21,48 +22,91 @@ enum SchemeType: Int, CaseIterable {
 
 struct SettingView: View {
     @Environment(\.dismiss) var dismiss
-    @Environment(\.colorScheme) var currentColorScheme
-    @AppStorage("appTheme") private var selectedTheme: Int = SchemeType.system.rawValue
+    @Environment(\.colorScheme) var colorScheme
+    @AppStorage("systemTheme") private var systemTheme: Int = SchemeType.allCases.first!.rawValue
     
-    var body: some View {
-        HStack {
-            Image(systemName: "circle.lefthalf.filled")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 24, height: 24)
-                .foregroundColor(.secondary)
-                .padding(.trailing)
-            
-            Picker(selection: $selectedTheme, label: Text("테마")) {
-                ForEach(SchemeType.allCases, id: \.self) { item in
-                    Text(item.schemeType)
-                        .tag(item.rawValue)
-                }
-            }
-//            .onChange(of: selectedTheme) { _ in
-//                applyTheme()
-//            }
+    @State var isShowingAppInfo: Bool = false
+    @State var isShowingContact: Bool = false
+    
+    var selectedScheme: ColorScheme? {
+        guard let theme = SchemeType(rawValue: systemTheme) else { return nil }
+        switch theme {
+        case .light:
+            return .light
+        case .dark:
+            return .dark
+        default:
+            return nil
         }
-//        .onAppear {
-//            applyTheme()
-//        }
     }
     
-//    private func applyTheme() {
-//        switch SchemeType(rawValue: selectedTheme) {
-//        case .system:
-//            // Use the system's color scheme
-//            break
-//        case .light:
-//            // Force light mode
-//            UIApplication.shared.windows.first?.overrideUserInterfaceStyle = .light
-//        case .dark:
-//            // Force dark mode
-//            UIApplication.shared.windows.first?.overrideUserInterfaceStyle = .dark
-//        case .none:
-//            break
-//        }
-//    }
+    var body: some View {
+    
+        List {
+            Section("앱 설정") {
+                HStack {
+                    Image(systemName: "paintpalette")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.secondary)
+                        .padding(.trailing)
+                    
+                    Picker(selection: $systemTheme, label: Text("테마")) {
+                        ForEach(SchemeType.allCases, id: \.self) { item in
+                            Text(item.schemeType)
+                                .tag(item.rawValue)
+                        }
+                    }
+                }
+                .preferredColorScheme(selectedScheme)
+            }
+            
+            Section("앱 정보") {
+                Button {
+                    isShowingAppInfo.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: "info.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 22, height: 22)
+                            .foregroundColor(.secondary)
+                            .padding(.trailing)
+                        
+                        Text("앱 정보")
+                            .foregroundColor(.primary)
+                    }
+                }
+                
+                Button {
+                    isShowingContact.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: "envelope")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 22, height: 22)
+                            .foregroundColor(.secondary)
+                            .padding(.trailing)
+                        
+                        Text("문의하기")
+                            .foregroundColor(.primary)
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingAppInfo) {
+                Text("앱 정보 들어갈 뷰")
+                    .presentationDetents([.medium, .fraction(0.75)])
+            }
+            .sheet(isPresented: $isShowingContact) {
+                Text("문의하기 정보 들어갈 뷰")
+                    .presentationDetents([.medium, .fraction(0.75)])
+            }
+        }
+    }
 }
 
 struct SettingView_Previews: PreviewProvider {
