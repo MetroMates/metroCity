26c1a364d2549014eedba15f8f3ed29829e149b2
swj <swjtwin@naver.com>
2023-12-05 01:45:15 +0900

    refactor: MainListVM StartVMSubscribe 수정.

--- Projects/App/Sources/Views/MainList/Architectures/MainListVM.swift
+++ Projects/App/Sources/Views/MainList/Architectures/MainListVM.swift
@@ -117,12 +117,14 @@ extension MainListVM {
                 self.subwayLineInfos = line
                 self.locationInfos = location
                 
-                // 초기 발행시 딱 한번 실행됨. -> 각 데이터 fetch가 완료되었을때 실행!
-                self.subscribe(stationInfo: station,
-                               lineInfo: line,
-                               locInfo: location)
-                
-                self.GPScheckNowLocactionTonearStation() // 내 위치 가져오기.
+                if !self.stationInfos.isEmpty, !self.subwayLineInfos.isEmpty, !self.locationInfos.isEmpty {
+                    // 초기 발행시 딱 한번 실행됨. -> 각 데이터 fetch가 완료되었을때 실행!
+                    self.subscribe(stationInfo: station,
+                                   lineInfo: line,
+                                   locInfo: location)
+                    
+                    self.GPScheckNowLocactionTonearStation() // 내 위치 가져오기.
+                }
                 
             }
             .store(in: &anyCancellable)
