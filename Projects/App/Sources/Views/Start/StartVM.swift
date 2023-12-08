e5b3197f02dab8c3e6cc8376e597022ceda6c010
swj <swjtwin@naver.com>
2023-12-08 22:16:19 +0900

    refactor: StartVM coreData DeleteAll return값 생략.

--- Projects/App/Sources/Views/Start/StartVM.swift
+++ Projects/App/Sources/Views/Start/StartVM.swift
@@ -65,9 +65,9 @@ final class StartVM: ObservableObject {
     private func deleteCoreData() {
         let coreDataManger = CoreDataManger.shared
         Log.trace("📝 CoreData Delete")
-        _ = coreDataManger.deleteAll(type: StationInfoEntity.self)
-        _ = coreDataManger.deleteAll(type: SubwayLineColorEntity.self)
-        _ = coreDataManger.deleteAll(type: StationLocationEntity.self)
+        coreDataManger.deleteAll(type: StationInfoEntity.self)
+        coreDataManger.deleteAll(type: SubwayLineColorEntity.self)
+        coreDataManger.deleteAll(type: StationLocationEntity.self)
     }
     
     private func setCoreData(ver: Int, datas: [FireStoreCodable]...) {
